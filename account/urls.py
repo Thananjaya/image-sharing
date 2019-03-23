e3674191653c7d694836e45e874736abc671cf50
Thananjaya <thananjaya091993@gmail.com>
2019-03-23 22:55:47 +0530

    profile edit and user edit been added

--- account/urls.py
+++ account/urls.py
@@ -5,14 +5,15 @@ from . import views
 urlpatterns = [
 	# path('login/', views.login, name="login")
 	# path('', include('django.contrib.auth.urls')), can use this one instead of mentioning the below urls
-	path('', auth_views.LoginView.as_view(), name="login"),
-	path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-	path('password_change/', auth_views.PasswordChangeView.as_view(), name="password_change"),
-	path('password_change/success', auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
+	path('', auth_views.LoginView.as_view(), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+	path('password_change/success', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 	path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
   path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
   path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
   path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-  path('register/', views.registration, name="registration"),
-	path('dashboard', views.dashboard, name="dashboard")
+  path('register/', views.registration, name='registration'),
+  path('edit_profile/', views.edit, name='edit_profile'),
+	path('dashboard', views.dashboard, name='dashboard')
 ]
--- account/views.py
+++ account/views.py
@@ -7,7 +7,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, RegistrationForm
+from django.contrib import messages
+from .forms import LoginForm, RegistrationForm, UserEditForm, ProfileEditForm
+
 
 @login_required
 def dashboard(request):
@@ -35,7 +37,26 @@ def registration(request):
 		registration_form = RegistrationForm()
 		return render(request, 'account/registration_form.html', {'form': registration_form})
 
-# to study how django authentication works
+@login_required
+def edit(request):
+	"""
+		Since we are ussing two forms in a single view, we have to provide instance
+		Means, we have to specify which data has to go to which form
+	"""
+	if request.method == 'POST':
+		user_form = UserEditForm(instance= request.user, data=request.POST)
+		profile_form = ProfileEditForm(instance= request.user.profile, data=request.POST, files=request.FILES)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			return render(request, 'account/dashboard.html')
+	else:
+		user_form = UserEditForm()
+		profile_form = ProfileEditForm()
+		return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
+# just implemented to study how django authentication works
 # def login(request):
 # 	"""
 # 		View for user login
