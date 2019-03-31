8309d74b86507eb5b7d31afbe4938633758b5d1c
thananjaya <thananjaya091993@gmail.com>
2019-03-31 12:29:28 +0530

    addin image model

--- account/models.py
+++ account/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class Profile(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
 	date_of_birth = models.DateField(null = True, blank = True)
-	image = models.ImageField(upload_to= 'users/%y/%m/%d/', blank=True, null=True)
+	image = models.ImageField(upload_to= 'users/%y/%m/%d/', blank = True, null = True)
 
 	def __str__(self):
 		return 'Profile of {}'.format(self.user.username)
